26524d1b3e2403c0e7815277640903ed0fc58948
toad2310 <meelisa609@gmail.com>
2024-03-18 17:28:13 +0300

    completed

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,122 +6,106 @@ const std::string API_ARGUMENTS = "/json/";
 namespace http = boost::beast::http;
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-    connect(ui->send, SIGNAL(clicked()), this, SLOT(Check()));
-    connect(ui->ip_input, SIGNAL(returnPressed()), this, SLOT(Check()));
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+  connect(ui->send, SIGNAL(clicked()), this, SLOT(Check()));
+  connect(ui->ip_input, SIGNAL(returnPressed()), this, SLOT(Check()));
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
+MainWindow::~MainWindow() { delete ui; }
 
-void MainWindow::Check() {
-    ui->info->clear();
-    std::string ip_input = ui->ip_input->text().toStdString();
-    std::string res_str = getResponse(ip_input);
-    std::string reserve = "";
-    double lat = 0, lon = 0;
-    bool is_address = false;
+void MainWindow::Check() noexcept {
+  ui->info->clear();
+  const std::string ip_input = ui->ip_input->text().toStdString();
+  const std::string res_str = getResponse(ip_input);
+  std::string reserve = "";
+  double lat = 0, lon = 0;
 
-    if (ui->continent_check->isChecked())
-        FormatOutput(getFieldFromJson(res_str, "continent"), "continent");
-    if (ui->country_check->isChecked())
-        FormatOutput(getFieldFromJson(res_str, "country"), "country");
-    if (ui->region_check->isChecked())
-        FormatOutput(getFieldFromJson(res_str, "region"), "region");
-    if (ui->city_check->isChecked())
-        FormatOutput(getFieldFromJson(res_str, "city"), "city");
-    if (ui->lat_check->isChecked()) {
-        reserve = getFieldFromJson(res_str, "lat");
-        if (reserve != "") {
-            lat = std::stod(reserve);
-            is_address = true;
-        }
-        FormatOutput(reserve, "lat");
+  if (ui->continent_check->isChecked())
+    FormatOutput(getFieldFromJson(res_str, "continent"), "continent");
+  if (ui->country_check->isChecked())
+    FormatOutput(getFieldFromJson(res_str, "country"), "country");
+  if (ui->region_check->isChecked())
+    FormatOutput(getFieldFromJson(res_str, "region"), "region");
+  if (ui->city_check->isChecked())
+    FormatOutput(getFieldFromJson(res_str, "city"), "city");
+  if (ui->lat_check->isChecked()) {
+    reserve = getFieldFromJson(res_str, "lat");
+    if (reserve != "") {
+      lat = std::stod(reserve);
     }
-    if (ui->lon_check->isChecked()) {
-        reserve = getFieldFromJson(res_str, "lon");
-        if (reserve != "") {
-            lon = std::stod(reserve);
-            is_address = true;
-        }
-        FormatOutput(reserve, "lon");
+    FormatOutput(reserve, "lat");
+  }
+  if (ui->lon_check->isChecked()) {
+    reserve = getFieldFromJson(res_str, "lon");
+    if (reserve != "") {
+      lon = std::stod(reserve);
     }
-    if (ui->timezone_check->isChecked())
-        FormatOutput(getFieldFromJson(res_str, "timezone"), "timezone");
-    if (ui->name_check->isChecked())
-        FormatOutput(getFieldFromJson(res_str, "asname"), "asname");
+    FormatOutput(reserve, "lon");
+  }
+  if (ui->timezone_check->isChecked())
+    FormatOutput(getFieldFromJson(res_str, "timezone"), "timezone");
+  if (ui->name_check->isChecked())
+    FormatOutput(getFieldFromJson(res_str, "asname"), "asname");
 
-    if (is_address)
-        OpenBrowser(lat, lon);
+  if (lat != 0.0 && lon != 0.0)
+    OpenBrowser(lat, lon);
 }
 
-std::string MainWindow::getResponse(std::string ip) {
-    boost::asio::io_context io;
-    boost::asio::ip::tcp::resolver resolver(io);
-    boost::asio::ip::tcp::socket socket(io);
+std::string MainWindow::getResponse(std::string ip) noexcept {
+  boost::asio::io_context io;
+  boost::asio::ip::tcp::resolver resolver(io);
+  boost::asio::ip::tcp::socket socket(io);
 
-    boost::asio::connect(socket, resolver.resolve(MAIN_API, "80"));
-    std::string argument = API_ARGUMENTS + ip;
+  boost::asio::connect(socket, resolver.resolve(MAIN_API, "80"));
+  const std::string argument = API_ARGUMENTS + ip;
 
-    http::request<http::string_body> req(http::verb::get, argument, 11);
+  http::request<http::string_body> req(http::verb::get, argument, 11);
 
-    req.set(http::field::host, MAIN_API);
-    req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+  req.set(http::field::host, MAIN_API);
+  req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
 
-    http::write(socket, req);
+  http::write(socket, req);
 
-    std::string response;
-    {
-        boost::beast::flat_buffer buffer;
-        http::response<http::dynamic_body> res;
-        http::read(socket, buffer, res);
-        response = boost::beast::buffers_to_string(res.body().data());
+  std::string response;
+  {
+    boost::beast::flat_buffer buffer;
+    http::response<http::dynamic_body> res;
+    http::read(socket, buffer, res);
+    response = boost::beast::buffers_to_string(res.body().data());
+  }
+  socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
 
-    }
-    socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
-
-    return response;
+  return response;
 }
 
-std::string MainWindow::getFieldFromJson(std::string json, std::string field) {
-    std::stringstream jsonEncoded(json);
-    boost::property_tree::ptree root;
-    boost::property_tree::read_json(jsonEncoded, root);
-    boost::optional<boost::property_tree::ptree&> cont_node = root.get_child_optional(field);
+std::string MainWindow::getFieldFromJson(std::string json,
+                                         std::string field) noexcept {
+  std::stringstream jsonEncoded(json);
+  boost::property_tree::ptree root;
+  boost::property_tree::read_json(jsonEncoded, root);
+  boost::optional<boost::property_tree::ptree &> cont_node =
+      root.get_child_optional(field);
 
-    if (cont_node)
-        return root.get<std::string>(field);
-    else return "";
+  if (cont_node)
+    return root.get<std::string>(field);
+  else
+    return "";
 }
 
-void MainWindow::OpenBrowser(double lat, double lon) {
-    std::string command = "";
-    std::string OS = getOS();
-    std::string url = "https://geotree.ru/coordinates?lat=" + std::to_string(lat) + "&lon=" + std::to_string(lon) + "&z=15&c=";
-    if (OS == "mac" || OS == "linux")
-        command = "open " + url;
-    else if (OS == "windows")
-        command = "start " + url;
-    system(command.c_str());
+void MainWindow::OpenBrowser(double lat, double lon) noexcept {
+  const QString qurl = QString::fromStdString(
+      "https://geotree.ru/coordinates?lat=" + std::to_string(lat) +
+      "&lon=" + std::to_string(lon) + "&z=15&c=100,17.2");
+  const QUrl url(qurl);
+  QDesktopServices::openUrl(url);
 }
 
-void MainWindow::FormatOutput(std::string field, std::string name) {
-    if (field == "")
-        ui->info->setText(ui->info->text() + QString::fromStdString(name) + ": null" + '\n');
-    else ui->info->setText(ui->info->text() + QString::fromStdString(name) + ": " + QString::fromStdString(field) + '\n');
-}
-
-std::string MainWindow::getOS() {
-    #ifdef _WIN32
-        return "windows";
-    #elif __APPLE__
-        return "mac";
-    #elif __linux__
-        return "linux";
-    #endif
+void MainWindow::FormatOutput(std::string field, std::string name) noexcept {
+  if (field == "")
+    ui->info->setText(ui->info->text() + QString::fromStdString(name) +
+                      ": null" + '\n');
+  else
+    ui->info->setText(ui->info->text() + QString::fromStdString(name) + ": " +
+                      QString::fromStdString(field) + '\n');
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,16 +1,16 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QDesktopServices>
 #include <QMainWindow>
 
 #include <string>
-#include <cstdlib>
 
-#include <boost/beast.hpp>
 #include <boost/asio/connect.hpp>
 #include <boost/asio/ip/tcp.hpp>
-#include <boost/property_tree/ptree.hpp>
+#include <boost/beast.hpp>
 #include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -18,23 +18,21 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
-    std::string getResponse(std::string ip);
-    std::string getFieldFromJson(std::string json, std::string field);
-    void OpenBrowser(double lat, double lon);
-    void FormatOutput(std::string field, std::string name);
-    std::string getOS();
+  Ui::MainWindow *ui;
+  std::string getResponse(std::string ip) noexcept;
+  std::string getFieldFromJson(std::string json, std::string field) noexcept;
+  void OpenBrowser(double lat, double lon) noexcept;
+  void FormatOutput(std::string field, std::string name) noexcept;
 
 private slots:
-    void Check();
+  void Check() noexcept;
 };
 #endif // MAINWINDOW_H
